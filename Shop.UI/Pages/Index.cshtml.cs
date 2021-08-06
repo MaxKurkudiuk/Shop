492e517152b1cf84ea6638227254be604b0c3c3a
Maksym Kurkudiuk <mkurkudiuk@deloitte.com>
2021-08-06 13:35:56 +0300

    Adding Products

--- Shop.UI/Pages/Index.cshtml.cs
+++ Shop.UI/Pages/Index.cshtml.cs
@@ -1,21 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Shop.Application.Products;
+using Shop.Database;
 using System.Threading.Tasks;
 
 namespace Shop.UI.Pages {
     public class IndexModel : PageModel {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
+        [BindProperty]
+        public ProductViewModel Product { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger) {
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context) {
             _logger = logger;
+            _context = context;
+        }
+
+        public class ProductViewModel {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
         }
 
         public void OnGet() {
 
+        }
+
+        public async Task<IActionResult> OnPost() {
+            await new CreateProduct(_context).Do(Product.Name, Product.Description, Product.Value);
+
+            return RedirectToPage("Index");
         }
     }
 }
