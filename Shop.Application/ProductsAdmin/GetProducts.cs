c211e4bd0a7a61a5f6f3401136a6e285ccb27599
Maksym Kurkudiuk <mkurkudiuk@deloitte.com>
2021-08-16 20:39:05 +0300

    Remove description

--- Shop.Application/ProductsAdmin/GetProducts.cs
+++ Shop.Application/ProductsAdmin/GetProducts.cs
@@ -13,14 +13,12 @@ namespace Shop.Application.ProductsAdmin {
         public IEnumerable<ProductViewModel> Do() => _context.Products.ToList().Select(x => new ProductViewModel {
             Id = x.Id,
             Name = x.Name,
-            Description = x.Description,
             Value = x.Value
         });
 
         public class ProductViewModel {
             public int Id { get; set; }
             public string Name { get; set; }
-            public string Description { get; set; }
             public decimal Value { get; set; }
         }
     }
