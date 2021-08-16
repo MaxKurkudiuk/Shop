2a67a21fda141251c3f737224f2283dcc7d98527
Maksym Kurkudiuk <mkurkudiuk@deloitte.com>
2021-08-16 20:49:15 +0300

    Update & Delete Products

--- Shop.UI/wwwroot/js/admin/main.js
+++ Shop.UI/wwwroot/js/admin/main.js
@@ -2,14 +2,39 @@
     el: '#app',
     data: {
         loading: false,
+        objectIndex: 0,
         productModel: {
+            id: 0,
             name: "Product Name",
             description: "Product Description",
-            value: 1.99
+            value: 0.00
         },
         products: []
     },
+    mounted() {  /*On first run (automatically)*/
+        this.getProducts();
+    },
     methods: {
+        getProduct(id) {
+            this.loading = true;
+            axios.get('/Admin/products/' + id)
+                .then(res => {
+                    console.log(res);
+                    var product = res.data;
+                    this.productModel = {
+                        id: product.id,
+                        name: product.name,
+                        description: product.description,
+                        value: product.value
+                    };
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
         getProducts() {
             this.loading = true;
             axios.get('/Admin/products')
@@ -18,7 +43,6 @@
                     this.products = res.data;
                 })
                 .catch(err => {
-                    console.log(url);
                     console.log(err);
                 })
                 .then(() => {
@@ -38,6 +62,38 @@
                 .then(() => {
                     this.loading = false;
                 });
+        },
+        updateProduct() {
+            this.loading = true;
+            axios.put('/Admin/products', this.productModel)
+                .then(res => {
+                    console.log(res.data);
+                    this.products.splice(this.objectIndex, 1, res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        deleteProduct(id, index) {
+            this.loading = true;
+            axios.delete('/Admin/products/' + id)
+                .then(res => {
+                    console.log(res);
+                    this.products.splice(index, 1);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        editProduct(id, index) {
+            this.objectIndex = index;
+            this.getProduct(id);
         }
     },
     computed: {
