6253ff5dae4f19f387e3aa4daccd724739d5b4ca
Maksym Kurkudiuk <mkurkudiuk@deloitte.com>
2021-08-16 20:32:26 +0300

    Fix UpdateProduct logic

--- Shop.Application/ProductsAdmin/UpdateProduct.cs
+++ Shop.Application/ProductsAdmin/UpdateProduct.cs
@@ -1,4 +1,5 @@
 ﻿using Shop.Database;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Shop.Application.ProductsAdmin {
@@ -10,11 +11,23 @@ namespace Shop.Application.ProductsAdmin {
         }
 
         public async Task<Response> Do(Request request) {
+            var product = _context.Products.FirstOrDefault(x => x.Id == request.Id);
+
+            product.Name = request.Name;
+            product.Description = request.Description;
+            product.Value = request.Value;
+
             await _context.SaveChangesAsync();
-            return new Response();
+            return new Response() {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Value = product.Value
+            };
         }
 
         public class Request {
+            public int Id { get; set; }
             public string Name { get; set; }
             public string Description { get; set; }
             public decimal Value { get; set; }
