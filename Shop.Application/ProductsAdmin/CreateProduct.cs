c79a60c64503bf6a3f924ee5b5bf952ff1ca0979
Maksym Kurkudiuk <mkurkudiuk@deloitte.com>
2021-08-16 18:18:51 +0300

    Get & Create Products

--- Shop.Application/ProductsAdmin/CreateProduct.cs
+++ Shop.Application/ProductsAdmin/CreateProduct.cs
@@ -10,17 +10,33 @@ namespace Shop.Application.ProductsAdmin {
             _context = context;
         }
 
-        public async Task Do(ProductViewModel vm) {
-            _context.Products.Add(new Product {
-                Name = vm.Name,
-                Description = vm.Description,
-                Value = vm.Value
-            });
+        public async Task<Response> Do(Request request) {
+            var product = new Product {
+                Name = request.Name,
+                Description = request.Description,
+                Value = request.Value
+            };
+
+            _context.Products.Add(product);
 
             await _context.SaveChangesAsync();
+
+            return new Response {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Value = product.Value
+            };
         }
 
-        public class ProductViewModel {
+        public class Request {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+        }
+
+        public class Response {
+            public int Id { get; set; }
             public string Name { get; set; }
             public string Description { get; set; }
             public decimal Value { get; set; }
--- Shop.UI/wwwroot/js/admin/main.js
+++ Shop.UI/wwwroot/js/admin/main.js
@@ -2,21 +2,37 @@
     el: '#app',
     data: {
         loading: false,
+        productModel: {
+            name: "Product Name",
+            description: "Product Description",
+            value: 1.99
+        },
         products: []
     },
     methods: {
         getProducts() {
             this.loading = true;
-            axios.get(url = '/Admin/products', {
-                url: url,
-                method: 'get',
-            })
+            axios.get('/Admin/products')
                 .then(res => {
                     console.log(res);
                     this.products = res.data;
                 })
                 .catch(err => {
                     console.log(url);
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        createProduct() {
+            this.loading = true;
+            axios.post('/Admin/products', this.productModel)
+                .then(res => {
+                    console.log(res.data);
+                    this.products.push(res.data);
+                })
+                .catch(err => {
                     console.log(err);
                 })
                 .then(() => {
