a9abe48b404b2e16386e9062c7f6aef32ba07d77
Maksym Kurkudiuk <mkurkudiuk@deloitte.com>
2021-08-19 12:22:07 +0300

    Add Stock

---
+++ Shop.Application/StockAdmin/DeleteStock.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Database;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Application.StockAdmin {
+    public class DeleteStock {
+        private ApplicationDbContext _context;
+
+        public DeleteStock(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public async Task<bool> Do(int id) {
+            var stock = _context.Stock.FirstOrDefault(x => x.Id == id);
+            _context.Stock.Remove(stock);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ Shop.Application/StockAdmin/GetStocks.cs
@@ -0,0 +1,29 @@
+﻿using Shop.Database;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop.Application.StockAdmin {
+    public class GetStocks {
+        private ApplicationDbContext _context;
+
+        public GetStocks(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public IEnumerable<StockViewModel> Do(int productId) 
+            => _context.Stock
+                .Where(x => x.ProductId == productId)
+                .Select(x => new StockViewModel() {
+                    Id = x.Id,
+                    Description = x.Description,
+                    Qty = x.Qty
+                })
+                .ToList();
+
+        public class StockViewModel {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public int Qty { get; set; }
+        }
+    }
+}
