cf0e63c074d190f39eaaf0e33ff5e18963c9d0ec
Maksym Kurkudiuk <mkurkudiuk@deloitte.com>
2021-08-26 15:35:01 +0300

    Add simple View component

---
+++ Shop.UI/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Cart;
+using Shop.Database;
+
+namespace Shop.UI.ViewComponents {
+    public class CartViewComponent : ViewComponent {
+        private ApplicationDbContext _context;
+
+        public CartViewComponent(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke() {
+            return View(new GetCart(HttpContext.Session, _context).Do());
+        }
+    }
+}
