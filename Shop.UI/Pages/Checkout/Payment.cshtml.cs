813001bbb875ca000b9c97085a7dd7c962efd190
Maksym Kurkudiuk <mkurkudiuk@deloitte.com>
2021-08-30 13:28:21 +0300

    Add CreateOrders

--- Shop.UI/Pages/Checkout/Payment.cshtml.cs
+++ Shop.UI/Pages/Checkout/Payment.cshtml.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Shop.Application.Cart;
+using Shop.Application.Orders;
 using Shop.Database;
 using Stripe;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Shop.UI.Pages.Checkout {
     public class PaymentModel : PageModel
@@ -23,7 +26,7 @@ namespace Shop.UI.Pages.Checkout {
             return Page();
         }
 
-        public IActionResult OnPost(string stripeEmail, string stripeToken) {
+        public async Task<IActionResult> OnPost(string stripeEmail, string stripeToken) {
             var customers = new CustomerService();
             var charges = new ChargeService();
 
@@ -39,6 +42,25 @@ namespace Shop.UI.Pages.Checkout {
                 Description = "Shop Purchase",      // Form title
                 Currency = "usd",                   // currency type
                 Customer = customer.Id              // customer in current session
+            });
+
+            // create order
+            await new CreateOrder(_context).Do(new CreateOrder.Request() {
+                StripeReference = charge.OrderId,
+
+                FirstName = cartOrder.CustomerInformation.FirstName,
+                LastName = cartOrder.CustomerInformation.LastName,
+                Email = cartOrder.CustomerInformation.Email,
+                PhoneNumber = cartOrder.CustomerInformation.PhoneNumber,
+                Address1 = cartOrder.CustomerInformation.Address1,
+                Address2 = cartOrder.CustomerInformation.Address2,
+                City = cartOrder.CustomerInformation.City,
+                PostCode = cartOrder.CustomerInformation.PostCode,
+
+                Stocks = cartOrder.Products.Select(x => new CreateOrder.Stock() {
+                    StockId = x.StockId,
+                    Qty = x.Qty
+                }).ToList()
             });
 
             return RedirectToPage("/Index");
